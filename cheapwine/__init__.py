54afba5469704fa24c93b8396c1891fd24c06185
HeapHeapHooray <heapheaphooray@hotmail.com>
2026-07-20 00:45:15 -0300

    Fix version reporting by dynamically resolving package version and updating release workflow

--- cheapwine/__init__.py
+++ cheapwine/__init__.py
@@ -2,4 +2,8 @@
 cheapwine: A lightweight, project-based Wine prefix and application manager.
 """
 
-__version__ = "0.1.0"
+try:
+    from importlib.metadata import version, PackageNotFoundError
+    __version__ = version("cheapwine")
+except Exception:
+    __version__ = "0.1.8"
